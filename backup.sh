38cdbaddaf966ed4cc2103e68c2ff9021f13b640
Bendingsyntax <github@bendingsyntax.com>
2022-05-09 18:08:40 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rsync -auzhAXP --exclude=/proc/* --exclude=/var/* / root@10.14.140.37:/root/Path/to/folder
+rsync -auzhAXP --exclude=/proc/* --exclude=/var/* / root@IP-ADDRESS:/root/Path/to/folder
 echo "Backup Completed : "$(date) >> /root/backs-completed.txt
